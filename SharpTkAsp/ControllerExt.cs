4c5f261a5b769681598690f7f157db6d521897bc
hi-ko-wi <hi-ko-wi@users.noreply.github.com>
2017-04-02 10:18:34 +0200

    Controller extension methods for auto-selecting NotFound, Empty and Ok

---
+++ SharpTkAsp/ControllerExt.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// Copyright (c) 2017-2017
+// for information on the creator and copyright owner, please see the author list bellow and the assembly 
+// information file. 
+// -
+// All rights are reserved. Reproduction or transmission in whole or in part, any form or by any means, electronic, 
+// mechanical or otherwise, is prohibited without the prior written consent of the copyright owner.
+// -
+// File:     ControllerExt.cs
+// Part of:  Tranquility
+// -
+// Author:   Haiko Wick (Haiko Wick)
+// Modified: 2017-04-02 10:13
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SharpTkAsp
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public static class ControllerExt
+    {
+        public static IActionResult OkOrNotFound<TIn>(this Controller ctl, TIn input)
+            where TIn : class
+            => input == null ? (IActionResult) ctl.NotFound() : ctl.Ok(value: input);
+
+        public static IActionResult OkOrNotFound<TIn>(this Controller ctl, TIn input, Func<TIn, object> rspFac)
+            where TIn : class
+            => input == null ? (IActionResult) ctl.NotFound() : ctl.Ok(rspFac(arg: input));
+
+        public static IActionResult OkOrNotFoundOrEmpty<TIn>(this Controller ctl, IEnumerable<TIn> input)
+            => input == null ? ctl.NotFound() : (!input.Any() ? (IActionResult) ctl.NoContent() : ctl.Ok(value: input));
+    }
+}
