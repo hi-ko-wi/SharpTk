a874c5dbc5672914528540f65da70cb119d879e4
hi-ko-wi <hi-ko-wi@users.noreply.github.com>
2018-04-11 07:46:29 +0200

    Secure hashing and hash validation function(s)

--- SharpTk/StringExt.cs
+++ SharpTk/StringExt.cs
@@ -7,16 +7,23 @@
 // mechanical or otherwise, is prohibited without the prior written consent of the copyright owner.
 // -
 // File:     StringExt.cs
-// Part of:  Tranquility
+// Part of:  SharpTk
 // -
 // Author:   Haiko Wick (Haiko Wick)
-// Modified: 2017-04-02 08:57
+// Modified: 2017-10-15 // 08:51
 // --------------------------------------------------------------------------------------------------------------------
-
-using System.Linq;
 
 namespace SharpTk
 {
+    #region
+
+    using System;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+
+    #endregion
+
     public static class StringExt
     {
         /// <summary>
@@ -34,9 +41,51 @@ namespace SharpTk
         /// <param name="input">Pascal-case input string.</param>
         /// <returns>A lowercase, underscore separated string.</returns>
         public static string PascalToLowerUnderscore(string input)
-            => string.Concat(
+            => string
+                .Concat(
                     input.Select(
                         selector: (x, i) => i > 0 && char.IsUpper(c: x) ? "_" + x.ToString() : x.ToString()))
                 .ToLower();
+
+        /// <summary>
+        ///     Securely hashes the given input string using HMAC
+        /// </summary>
+        /// <param name="input">Input string to compute a hash for.</param>
+        /// <returns>The computed hash as a hex string.</returns>
+        public static (string salt, string hash) SecureHash(this string input)
+        {
+            byte[] salt;
+            using (var rng = new RNGCryptoServiceProvider()) {
+                var rawSalt = new byte[128 / 8];
+                rng.GetBytes(data: rawSalt);
+                salt = rawSalt;
+            }
+
+            var hmac = new HMACSHA256(key: salt);
+            var hash = hmac.ComputeHash(Encoding.Default.GetBytes(s: input));
+
+            var saltString = Convert.ToBase64String(inArray: salt);
+            var hashString = Convert.ToBase64String(inArray: hash);
+
+            return (saltString, hashString);
+        }
+
+        /// <summary>
+        ///     Compares an input string with an expected hashed secret.
+        /// </summary>
+        /// <param name="input">The input string to check.</param>
+        /// <param name="salt">The salt used to create the hash previously.</param>
+        /// <param name="expectedHash">The known hashed value to check the input with.</param>
+        /// <returns></returns>
+        public static bool SecureHashValidate(this string input, string salt, string expectedHash)
+        {
+            var saltBytes = Convert.FromBase64String(s: salt);
+
+            var hmac = new HMACSHA256(key: saltBytes);
+            var hash = hmac.ComputeHash(Encoding.Default.GetBytes(s: input));
+            var secretBytes = Convert.FromBase64String(s: expectedHash);
+
+            return secretBytes.SequenceEqual(second: hash);
+        }
     }
 }
